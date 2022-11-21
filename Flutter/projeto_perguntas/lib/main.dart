a1cd917de9fb7978f53f0a905db832e4237dde1e
Marcos André <marcos32andre@gmail.com>
2022-11-21 19:51:25 -0300

    aa

--- Flutter/projeto_perguntas/lib/main.dart
+++ Flutter/projeto_perguntas/lib/main.dart
@@ -1,44 +1,63 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
+import './resposta.dart';
 
 void main() => runApp(const PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+   final List<Map<String, Object>> _perguntas = const [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'Respostas': ['Preto', 'Vermelho', 'Azul', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'Respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'Respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      }
+    ];
+
   void _responder() {
-    setState(() {
+    if(temPerguntaSelecionada) {
+      setState(() {
       _perguntaSelecionada++;
     });
-    print(_perguntaSelecionada);
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-    ];
-
+   
+  
+  List <String> respostas = temPerguntaSelecionada
+  ? _perguntas[_perguntaSelecionada] ['Respostas'] as List <String>
+  : [ ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: Column(
+        body: temPerguntaSelecionada 
+        ? Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(
-              onPressed: _responder,
-              child: const Text('Resposta 1'),
-            ),
-            ElevatedButton(
-              onPressed: _responder,
-              child: const Text('Resposta 2'),
-            ),
-            ElevatedButton(
-                onPressed: _responder, child: const Text('Resposta 3'))
+            Questao(_perguntas[_perguntaSelecionada]['texto'] as String),
+            ...respostas.map((texto) => Resposta(texto, _responder)).toList(),
           ],
+        )
+        : const Center(
+          child: Text(
+            'Parabéns!',
+            style: TextStyle(fontSize: 28),
+          ),
         ),
       ),
     );
